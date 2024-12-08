e8d66c6ac43ddc6543b0e8797cfee8b5ab414c5f
Siileenttt <prasannapk17@gmail.com>
2024-12-08 11:01:19 +0530

    second commit

---
+++ odd even number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Console.WriteLine("enter the number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            if (Number % 2 == 0) 
+                Console.WriteLine("It is Even Number");
+            else
+                Console.WriteLine("it is Odd number");
+            goto start;
+            Console.Read();
+        }
+    }
+}
