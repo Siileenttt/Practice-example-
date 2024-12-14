cefc87d2aa8fe1ffa9dd701c88c481e47e1f52bb
Prasanna kharche <148361046+Siileenttt@users.noreply.github.com>
2024-12-14 17:44:50 +0530

    Add files via upload

---
+++ Data_Type.cs
@@ -0,0 +1,32 @@
+using System;
+namespace DataType
+{
+	class program
+	{
+		static void Main()
+		{
+			sbyte a=sbyte.MinValue;
+			sbyte b=sbyte.MaxValue;
+			byte c=byte.MinValue;
+			byte d=byte.MaxValue
+			short e=short.MinValue;
+			short f=short.MaxValue;
+			int g=int.MinValue;
+			int j=int.MaxValue;
+			double h=double.MinValue;
+			double I=double.MaxValue;
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+			Console.WriteLine(c);
+			Console.WriteLine(d);
+			Console.WriteLine(e);
+			Console.WriteLine(f);
+			Console.WriteLine(g);
+			Console.WriteLine(j);
+			Console.WriteLine(h);
+			Console.WriteLine(i);
+		}
+	}
+}
+
+			
