379257f0759598cd72d1b0d321327d2f7d8e11c3
Prasanna kharche <148361046+Siileenttt@users.noreply.github.com>
2025-12-16 12:41:43 +0530

    Add files via upload

---
+++ Example_1.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+void main()
+{
+	int on, ink=4,act=1;
+	float tig=3.2;
+	on = ink * act / 2 + 3 / 2 * act + 2 + tig ; 
+	printf("%d",on);
+}
---
+++ Example_3.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+void main()
+{
+
+	int x=3;
+	float y=3.0;
+	
+	if(x==y)
+		printf("\nX and y is equal");
+	else
+		printf("\n x and y is not equal");
+	
+} 
+
